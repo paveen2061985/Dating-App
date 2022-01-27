9cd89b180ee6b254ced4ca3b7ffb30a06a79bf73
Praveen <praveenreddy206@gmail.com>
2022-01-27 06:09:02 +0530

    Added Register and Login Api

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+        public class BaseApiController : ControllerBase
+    {
+       
+
+    }
+}
